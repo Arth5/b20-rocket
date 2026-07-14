e0eaf1f2df9bf6e883ba5c8f31105b284f05eee8
Arthz <helmo.arturo@gmail.com>
2026-07-13 23:16:22 -0500

    Configure ERC20 token constructor and ownership

--- contracts/src/B20Token.sol
+++ contracts/src/B20Token.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract B20Token {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract B20Token is ERC20, Ownable {
+    constructor()
+        ERC20("B20 Rocket", "B20")
+        Ownable(msg.sender)
+    {
+
+    }
 }
