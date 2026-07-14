56a79ec37018f56c7f5de92f1d2d93a0652948ac
Arthz <helmo.arturo@gmail.com>
2026-07-13 20:20:56 -0500

    Configure Base Sepolia network

--- src/providers/Web3Provider.tsx
+++ src/providers/Web3Provider.tsx
@@ -4,13 +4,13 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { base } from "wagmi/chains";
+import { baseSepolia } from "wagmi/chains";
 import { WagmiProvider } from "wagmi";
 
 const config = getDefaultConfig({
   appName: "B20 Rocket",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [base],
+  chains: [baseSepolia],
   ssr: true,
 });
 
