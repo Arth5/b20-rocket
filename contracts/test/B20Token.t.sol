7f0a6dfb11a5f523d908cc6c37e449e24718e500
Arthz <helmo.arturo@gmail.com>
2026-07-14 22:17:20 -0500

    Add initial token configuration test

---
+++ contracts/test/B20Token.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {B20Token} from "../src/B20Token.sol";
+
+contract B20TokenTest is Test {
+    B20Token internal token;
+
+    address internal owner = address(0xA11CE);
+
+    function setUp() public {
+        token = new B20Token(
+            "B20 Rocket",
+            "B20",
+            1_000_000,
+            owner
+        );
+    }
+
+    function testInitialTokenConfiguration() public view {
+        assertEq(token.name(), "B20 Rocket");
+        assertEq(token.symbol(), "B20");
+        assertEq(token.decimals(), 18);
+        assertEq(token.owner(), owner);
+        assertEq(token.totalSupply(), 1_000_000 * 10 ** 18);
+        assertEq(token.balanceOf(owner), 1_000_000 * 10 ** 18);
+    }
+}
