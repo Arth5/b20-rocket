ec2ead5fcbb7cb73bcc1aa62f827de9c6373e958
Arthz <helmo.arturo@gmail.com>
2026-07-17 21:45:21 -0500

    Add state restoration test with different mint amount

--- contracts/test/B20Token.t.sol
+++ contracts/test/B20Token.t.sol
@@ -655,4 +655,22 @@ contract B20TokenTest is Test {
         assertEq(token.balanceOf(owner), 50 * 10 ** token.decimals());
         assertEq(token.totalSupply(), 50 * 10 ** token.decimals());
     }
+
+    function testMintAfterFullBurnRestoresStateWithDifferentAmount() public {
+        uint256 ownerBalance = token.balanceOf(owner);
+        uint256 amountToBurn = ownerBalance / (10 ** token.decimals());
+
+        vm.startPrank(owner);
+        token.burn(amountToBurn);
+
+        assertFalse(token.exists(owner));
+        assertEq(token.totalSupply(), 0);
+
+        token.mint(owner, 125);
+        vm.stopPrank();
+
+        assertTrue(token.exists(owner));
+        assertEq(token.balanceOf(owner), 125 * 10 ** token.decimals());
+        assertEq(token.totalSupply(), 125 * 10 ** token.decimals());
+    }
 }
